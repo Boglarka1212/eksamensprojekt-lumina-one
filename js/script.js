0ec8bbe3fd829c2fc89d8530eee497bd03c536b4
boglarka.kazmer <kazmer.boglarka@gmail.com>
2026-05-23 15:12:53 +0200

    element fundet i dommen og ekstra key-value tilføjet i arrayet

--- js/script.js
+++ js/script.js
@@ -5,30 +5,41 @@ const features = [
     title: "1. Holder hele dagen",
     text: "Ingen stress over batteri midt i stemningen. Lumina følger jer fra morgen til aften.",
     image: "img/holder-hele-dagen.png",
+    caption: "Holder dagen lang",
   },
   {
     title: "2. Med helt ned til vandet",
     text: "Du kan tage den med tæt på vandet uden at skulle passe på den - det kan den holde til!",
     image: "img/ned-til-vandet.png",
+    caption: "Kan tåle splash fra vand",
   },
   {
     title: "3. Smuk nok til at stå fremme",
     text: "Bløde former, rolige farver og et udtryk, der føles mere som interiør end klassisk tech.",
     image: "img/smuk-nok.png",
+    caption: "Står smukt på hylden",
   },
   {
     title: "4. Lyden, som samler jer",
     text: "Kraftig nok til at fylde rummet, men stadig blød nok til samtaler, grin og hygge.",
     image: "img/lyden-samler-jer.png",
+    caption: "Samlet rundt om højtaleren",
   },
   {
     title: "5. Let at tage med overalt",
     text: "Let nok til at komme med, også når tasken allerede er fyldt.",
     image: "img/let-som-fjer.png",
+    caption: "Let som fjer",
   },
   {
     title: "6. Én playliste",
     text: "Med Social Connect kan veninderne blande deres musik ind, så stemningen ikke kun er én persons ansvar.",
     image: "img/en-playliste.png",
+    caption: "Blend jeres playlister",
   },
 ];
+
+
+/* Finder pladsen til cards i dommen, elementet der hedder feature-list */
+const featureList = document.getElementById("feature-list");
+
