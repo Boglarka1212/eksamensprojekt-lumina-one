83b453faa9418081965dffa445418c1ad9868b75
boglarka.kazmer <kazmer.boglarka@gmail.com>
2026-05-23 14:22:38 +0200

    et array oprettet til feature cards

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const features = [
+  {
+    title: "1. Holder hele dagen",
+    text: "Ingen stress over batteri midt i stemningen. Lumina følger jer fra morgen til aften.",
+    image: "img/holder-hele-dagen.png",
+  },
+  {
+    title: "2. Med helt ned til vandet",
+    text: "Du kan tage den med tæt på vandet uden at skulle passe på den - det kan den holde til!",
+    image: "img/ned-til-vandet.png",
+  },
+  {
+    title: "3. Smuk nok til at stå fremme",
+    text: "Bløde former, rolige farver og et udtryk, der føles mere som interiør end klassisk tech.",
+    image: "img/smuk-nok.png",
+  },
+  {
+    title: "4. Lyden, som samler jer",
+    text: "Kraftig nok til at fylde rummet, men stadig blød nok til samtaler, grin og hygge.",
+    image: "img/lyden-samler-jer.png",
+  },
+  {
+    title: "5. Let at tage med overalt",
+    text: "Let nok til at komme med, også når tasken allerede er fyldt.",
+    image: "img/let-som-fjer.png",
+  },
+  {
+    title: "6. Én playliste",
+    text: "Med Social Connect kan veninderne blande deres musik ind, så stemningen ikke kun er én persons ansvar.",
+    image: "img/en-playliste.png",
+  },
+];
